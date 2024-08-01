f355f34ec76ef49128633880d6daa40c79dc1ee8
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-01 16:22:08 +0200

    Milestone 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // PLANNING
 
-/*
+/* BASIC VERS.
 Recupero gli elementi dal DOM / Get elements from the DOM
 Creare una variabile per il costo per chilometro / Make a price/distance var
 Creare una variabile per lo sconto / Make a discount var
@@ -12,12 +12,23 @@ Stampare il risultato / Output the result
 
 */
 
+/* MILESTONE 1
+Recupero gli elementi dal DOM / Get elements from the DOM
+
+*/
+
 // # STAGING
 
 // * Recupero gli elementi dal DOM / Get elements from the DOM
 
 const totalElement = document.getElementById('total')
 const discountElement = document.getElementById('discount')
+
+// ? Form
+
+const distanceInput = document.getElementById('distance')
+const ageInput = document.getElementById('age')
+const submitButton = document.getElementById('submit-button')
 
 // * Creare una variabile per il costo per chilometro / Make a price/distance var
 const priceUnit = 0.21;
@@ -32,56 +43,67 @@ let discount = null;
 
 // # DATA GATHERING 
 
-// * Chiedere il numero di chilometri da percorrere. / Request travel distance
-
-const distance = parseInt(prompt("Inserisci la distanza da percorrere (km)", 5))
-console.log('distance', distance);
-
-// * Chiedere l'età del passeggero. / Request traveller age
-
-const age = parseInt(prompt("Inserisci la tua età", 25));
-console.log('age', age);
 
 // @ VALIDATION
 
-if (isNaN(distance) || isNaN(age) || distance <= 0 || age <= 0) {
-    alert('I dati inseriti non sono validi')
-}
-else {
 
 
-    // # EVENT HANDLING
+// # EVENT HANDLING
+
+// if (isNaN(distance) || isNaN(age) || distance <= 0 || age <= 0) {
+//     alert('I dati inseriti non sono validi')
+// } else {
+
+submitButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+
+    // * Chiedere il numero di chilometri da percorrere. / Request travel distance
+
+    const distance = parseInt(distanceInput.value);
+    console.log('distance', distance);
+
+    // * Chiedere l'età del passeggero. / Request traveller age
+
+    const age = parseInt(ageInput.value);
+    console.log('age', age);
 
 
     // # PROCESSING
+    if (isNaN(distance) || isNaN(age) || distance <= 0 || age <= 0) {
+        alert('I dati inseriti non sono validi')
+    } else {
+
+        // * Calcolare il prezzo del biglietto senza sconto. / Calculate full price ticket
+
+        const ticketPrice = parseInt(distance * priceUnit);
+        let finalPrice = ticketPrice;
+        console.log('ticketPrice', ticketPrice);
+
+        // * Calcolare il prezzo del biglietto con lo sconto. / Calculate reduced price ticket
+
+        if (age >= 65) discount = 40;
+        else if (age < 18) discount = 20;
+
+        if (discount) {
+            const discountElement = document.getElementById('discount');
+            const discountMessage = `<b>Hai usufruito del ${discount}% di sconto.</b>`
+
+            discountElement.innerHTML = discountMessage;
+
+            discountAmount = (finalPrice / 100) * discount;
+
+            finalPrice -= discountAmount;
+            console.log('finalPrice', finalPrice.toFixed(2))
+        }
 
 
-    // * Calcolare il prezzo del biglietto senza sconto. / Calculate full price ticket
+        // # OUTPUT 
 
-    const ticketPrice = (distance * priceUnit);
-    let finalPrice = ticketPrice;
-    console.log('ticketPrice', ticketPrice);
+        // * Stampare il risultato / Output the result
 
-    // * Calcolare il prezzo del biglietto con lo sconto. / Calculate reduced price ticket
+        totalElement.innerHTML = message + finalPrice.toFixed(2);
 
-    if (age >= 65) discount = 40;
-    else if (age < 18) discount = 20;
-
-    if (discount) {
-        const discountElement = document.getElementById('discount');
-        const discountMessage = `<b>Hai usufruito del ${discount}% di sconto.</b>`
-
-        discountElement.innerHTML = discountMessage;
-
-        discountAmount = (finalPrice / 100) * discount;
-
-        finalPrice -= discountAmount;
     }
 
-
-    // # OUTPUT 
-
-    // * Stampare il risultato / Output the result
-
-    totalElement.innerHTML = message + finalPrice.toFixed(2);
-}
+})
